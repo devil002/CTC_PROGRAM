d795047896c4bdb79354baaf0ac1d4b9090c083e
Devis Vijith <devisvijith@gmail.com>
2021-05-27 12:56:48 +0530

    Update test5.py

--- test5.py
+++ test5.py
@@ -2,7 +2,7 @@ num = int(input())
 temp = []
 even = [int(i) for i in range(2,(num*2)+2) if i % 2 == 0]
 for i in range(0,num):
-    foo = ((i+1)*6*(i+1))+((i+1)-1)*even[i]
+    foo = (6*(i+1)**2)+i*even[i]
     temp.append(foo)
 # print(*even)
 print(*temp)
